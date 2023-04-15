a67375e5596eebe9da861b149a4aa61df3901c85
AHMED <hmoodhmood004@gmail.com>
2023-04-15 17:53:59 +0300

    best version

--- app/src/main/java/com/example/tut/MainActivity.kt
+++ app/src/main/java/com/example/tut/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import com.example.tut.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
@@ -12,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var Binding: ActivityMainBinding
     private var database: DatabaseReference? = null
     var data: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,27 +23,38 @@ class MainActivity : AppCompatActivity() {
         Binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_landing)
 
-        // Initialize Firebase Database
-        database = FirebaseDatabase.getInstance("https://voltagereadings-default-rtdb.europe-west1.firebasedatabase.app").getReference()
+        // Initialize Firebase Database query
+        val query = FirebaseDatabase.getInstance("https://voltageread-22aa9-default-rtdb.firebaseio.com/")
+            .reference
+            .child("all")
+            .orderByKey()
+            .limitToLast(1)
 
-        // Fetch data from Firebase Realtime Database and save it in a variable
-        database!!.addValueEventListener(object : ValueEventListener {
+        query.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.value != null) {
-                    data = snapshot.value.toString() // store the received data in the variable
-                    Log.d("Firebase", "Data received: $data")
-                    // Update the UI with the received data
-                    Binding.voltage.text = data
+                if (snapshot.exists() && snapshot.value != null) {
+                    val lastChildSnapshot = snapshot.children.lastOrNull()
+                    if (lastChildSnapshot != null) {
+                        val data = lastChildSnapshot.value.toString()
+                        if (!data.isNullOrEmpty()) {
+                            Log.d("Firebase", "Data received: $data")
+                            Binding.voltage.text = data.substringAfter('=')
+                        }
+                    }
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.e("Firebase", "Data fetching cancelled: ${error.message}")
+                Log.d("Firebase", "Error fetching data from Firebase: ${error.message}")
             }
         })
-        val myBtn=findViewById<Button>(R.id.btnToReadings)
+
+
+
+    val myBtn=findViewById<Button>(R.id.btnToReadings)
         myBtn.setOnClickListener {
-            setContentView(R.layout.activity_main)
+            setContentView(Binding.root)
+            print(data)
         }
     }
 }
